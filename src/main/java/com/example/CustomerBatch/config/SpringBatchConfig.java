3b329e28a81f685e56a16c5a95d6e11f6101c69c
dalissia <dalissiabenidir@gmail.com>
2024-09-08 21:05:14 +0200

    add JobController

--- src/main/java/com/example/CustomerBatch/config/SpringBatchConfig.java
+++ src/main/java/com/example/CustomerBatch/config/SpringBatchConfig.java
@@ -17,15 +17,15 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
-
 import java.io.IOException;
 
-import static com.fasterxml.jackson.databind.util.ClassUtil.name;
 
+@Configuration
 public class SpringBatchConfig {
     @Autowired
     CustomerRepository customerRepository;
